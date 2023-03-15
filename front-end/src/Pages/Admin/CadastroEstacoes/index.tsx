574667c9001c59dbc76546c45edba513b8ab17de
ViniciusAndBar <vini.zeus2002@gmail.com>
2023-03-15 17:43:45 -0300

    Criação dos campos da pagina utilizando PrimeReact

--- front-end/src/Pages/Admin/CadastroEstacoes/index.tsx
+++ front-end/src/Pages/Admin/CadastroEstacoes/index.tsx
@@ -10,34 +10,48 @@ interface Paramns {
     unidade: string;
 }
 
-
 function CadastroEstacao() {
     const [value, setValue] = useState<string>('');
-    const [selectedCities, setSelectedCities] = useState<Paramns | null>(null);
+    /* const [selectedParameters, setSelectedParameters] = useState<Paramns | null>(null);
     const Parametros: Paramns[] = [
         { name: 'Temperatura', unidade: '°C' },
         { name: 'Umidade', unidade: '%' },
         { name: 'Vento', unidade: 'Km/h' },
-    ];
+    ]; */
 
     return (
-        <>
-            <S.Container>
+        <S.Container>
+            <section>
+                <header>
+                    {/* destinado ao cabeçalho */}
+                </header>
                 <main>
-                    <div className="Login">
-
+                    <div className="card">
+                        <div className="campos">
+                            <h1>Cadastrar Estação</h1>
+                            <div className="estacaoNome">
+                                <label htmlFor="username">Nome da estação</label>
+                                <InputText className="inputNome" type="text" placeholder="Estação X" value={value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)} />
+                            </div>
+                            <div className="descricao">
+                                <label htmlFor="description">Descrição</label>
+                                <InputTextarea value={value} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setValue(e.target.value)} rows={8} />
+                            </div>
+                            <div className="localizacao">
+                                <label htmlFor="localization">Localização</label>
+                                <InputText type="text" placeholder="Localização" value={value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)} />
+                            </div>
+                            {/* <div className="parametros">
+                                <label htmlFor="Parameters">Parametros</label>
+                                <MultiSelect value={selectedParameters} onChange={(e: MultiSelectChangeEvent) => setSelectedParameters(e.value)} options={Parametros} optionLabel="name"
+                                    placeholder="Parametros" maxSelectedLabels={3} />
+                            </div> */}
+                        </div>
                     </div>
-                    <div>
-
-                    </div>
-                    <InputText type="text" placeholder="Username" value={value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)} />
-                    <InputTextarea autoResize value={value} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setValue(e.target.value)} rows={5} cols={30} />
-                    <InputText type="text" placeholder="Localização" value={value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)} />
-                    <MultiSelect value={selectedCities} onChange={(e: MultiSelectChangeEvent) => setSelectedCities(e.value)} options={Parametros} optionLabel="name"
-                        placeholder="Parametros" maxSelectedLabels={3} className="w-full md:w-20rem" />
                 </main>
-            </S.Container>
-        </>
+            </section>
+        </S.Container>
     )
 }
 export default CadastroEstacao;
+
--- front-end/src/Pages/Admin/CadastroEstacoes/styles.ts
+++ front-end/src/Pages/Admin/CadastroEstacoes/styles.ts
@@ -1,14 +1,77 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-display: flex;
-flex-direction: column;
-overflow-x: hidden;
-background-color: #f5f5f5;
+section{
 
-main{
-    margin-top: 3.5em;
-    display: flex;
+    header{
+        z-index: 100;
+        width: 100vw;
+        top: 0;
+        position: fixed;
+    }
+    main{
+        margin-top: 4em;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+
+        .card{
+            background-color: #FFFFFF;
+            width: 500px;
+            hight: 700px;
+
+            .campos{
+                display: flex;
+                flex-direction: column;
+                width: 25vw;
+                padding: 5%;
+    
+                .estacaoNome{
+                    display: flex;
+                    flex-direction: column;
+    
+                    label{
+                        align-items: left;
+                        text-align: left;
+                    }
+                }
+    
+                .descricao{
+                    display: flex;
+                    flex-direction: column;
+    
+                    label{
+                        align-items: left;
+                        text-align: left;
+                    }
+                }      
+    
+                .localizacao{
+                    display: flex;
+                    flex-direction: column;
+    
+                    label{
+                        align-items: left;
+                        text-align: left;
+                    }
+                }
+    
+                .parametros{
+                    display: flex;
+                    flex-direction: column;
+    
+                    label{
+                        align-items: left;
+                        text-align: left;
+                    }
+    
+                }
+            }
+        } 
+    }
 }
-
 `;
+
+/* cor de fundo para teste background-color: #0F6EFD; */
