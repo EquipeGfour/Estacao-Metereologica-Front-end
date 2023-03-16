b9baba6c77f1ae4d613a64ac6f26d147a093e9e8
ViniciusAndBar <vini.zeus2002@gmail.com>
2023-03-16 10:50:26 -0300

    Criado o cadastro d estações

--- front-end/src/Pages/Admin/CadastroEstacoes/index.tsx
+++ front-end/src/Pages/Admin/CadastroEstacoes/index.tsx
@@ -9,15 +9,29 @@ interface Paramns {
     name: string;
     unidade: string;
 }
+interface Alerts {
+    name: string;
+    unidade: string;
+}
 
 function CadastroEstacao() {
     const [value, setValue] = useState<string>('');
-    /* const [selectedParameters, setSelectedParameters] = useState<Paramns | null>(null);
+    const [valuee, setValuee] = useState<string>('');
+    const [values, setValues] = useState<string>('');
+    const [selectedParameters, setSelectedParameters] = useState<Paramns | null>(null);
+    const [selectedAlerts, setSelectedAlerts] = useState<Alerts | null>(null);
+
     const Parametros: Paramns[] = [
         { name: 'Temperatura', unidade: '°C' },
         { name: 'Umidade', unidade: '%' },
         { name: 'Vento', unidade: 'Km/h' },
-    ]; */
+    ];
+    const Alertas: Alerts[] = [
+        { name: 'Calor', unidade: '°C' },
+        { name: 'Chuva', unidade: 'mm' },
+        { name: 'Vento', unidade: 'Km/h' },
+
+    ];
 
     return (
         <S.Container>
@@ -28,24 +42,34 @@ function CadastroEstacao() {
                 <main>
                     <div className="card">
                         <div className="campos">
-                            <h1>Cadastrar Estação</h1>
+                            <p>Cadastrar Estação</p>
                             <div className="estacaoNome">
                                 <label htmlFor="username">Nome da estação</label>
                                 <InputText className="inputNome" type="text" placeholder="Estação X" value={value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)} />
                             </div>
                             <div className="descricao">
                                 <label htmlFor="description">Descrição</label>
-                                <InputTextarea value={value} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setValue(e.target.value)} rows={8} />
+                                <InputTextarea value={valuee} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setValuee(e.target.value)} rows={8} />
                             </div>
                             <div className="localizacao">
                                 <label htmlFor="localization">Localização</label>
-                                <InputText type="text" placeholder="Localização" value={value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)} />
+                                <InputText type="text" placeholder="Localização" value={values} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValues(e.target.value)} />
                             </div>
-                            {/* <div className="parametros">
-                                <label htmlFor="Parameters">Parametros</label>
-                                <MultiSelect value={selectedParameters} onChange={(e: MultiSelectChangeEvent) => setSelectedParameters(e.value)} options={Parametros} optionLabel="name"
-                                    placeholder="Parametros" maxSelectedLabels={3} />
-                            </div> */}
+                            <div className="parametros-e-alertas">
+                                <div className="parametros">
+                                    <label htmlFor="Parameters">Parametros</label>
+                                    <MultiSelect value={selectedParameters} onChange={(e: MultiSelectChangeEvent) => setSelectedParameters(e.value)} options={Parametros} optionLabel="name"
+                                        maxSelectedLabels={3} />
+                                </div>
+                                <div className="alertas">
+                                    <label htmlFor="Alerts">Alertas</label>
+                                    <MultiSelect value={selectedAlerts} onChange={(e: MultiSelectChangeEvent) => setSelectedAlerts(e.value)} options={Alertas} optionLabel="name"
+                                        maxSelectedLabels={3} />
+                                </div>
+                            </div>
+                            <div className="botao">
+                                <Button label="Cadastrar" type="submit" className="p-button-outlined" />
+                            </div>
                         </div>
                     </div>
                 </main>
--- front-end/src/Pages/Admin/CadastroEstacoes/styles.ts
+++ front-end/src/Pages/Admin/CadastroEstacoes/styles.ts
@@ -2,7 +2,13 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
 section{
+    color: #737479;
 
+    p{
+        margin-top: 0px;
+        font-size: 1.5vw;
+        color: #000;
+    }
     header{
         z-index: 100;
         width: 100vw;
@@ -16,9 +22,15 @@ section{
         justify-content: center;
         align-items: center;
         text-align: center;
-
         .card{
-            background-color: #FFFFFF;
+            margin-top: 3em ;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            border-radius: 8px;
+            box-shadow: 3px 1px 2px 2px gray, -1px 0 0.4px gray;
+            background-color: #ffffff;
             width: 500px;
             hight: 700px;
 
@@ -45,6 +57,7 @@ section{
                     label{
                         align-items: left;
                         text-align: left;
+                        margin-top: 5px;
                     }
                 }      
     
@@ -55,18 +68,42 @@ section{
                     label{
                         align-items: left;
                         text-align: left;
+                        margin-top: 5px;
                     }
                 }
     
-                .parametros{
+                .parametros-e-alertas{
                     display: flex;
-                    flex-direction: column;
+                    flex-direction: row;
+                    justify-content: space-between;
     
                     label{
                         align-items: left;
                         text-align: left;
+                        margin-top: 5px;
+
                     }
-    
+                    .parametros{
+                        display: flex;
+                        flex-direction: column;
+                        width: 48%;
+
+                    }
+                    .alertas{
+                        display: flex;
+                        flex-direction: column;
+                        width: 48%;
+                        
+                    }
+                }
+
+                .botao{
+                    display: flex;
+                    margin-top: 18px;
+                    
+                    Button{
+                        width: 100%;
+                    }
                 }
             }
         } 
