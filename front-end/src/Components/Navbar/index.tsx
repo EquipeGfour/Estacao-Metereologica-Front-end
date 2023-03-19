bac1c9b17a27887a3f00b0c6f47397806b13cadd
KevinFMfatec <mirendakevin@gmail.com>
2023-03-19 19:09:27 -0300

    Arrumando a página de Listagem das Estações

---
+++ front-end/src/Components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import * as S from "./styles";
+import React from 'react'; 
+import { Menubar } from 'primereact/menubar';
+import { MenuItem } from 'primereact/menuitem';
+import 'primeicons/primeicons.css';
+
+function Navbar () {
+    const start = <img alt="logo" src="https://tecsus.com.br/wp-content/uploads/2020/10/logo_tecsus_horizontal.png" height="40" className="tecsus"></img>;
+    const items: MenuItem[] = [{ 
+        label: 'Home',
+        icon: 'pi pi-fw pi-home'
+    }]
+    return(
+        <>
+            <S.NavBar>
+                <Menubar model={items} start={start} />
+            </S.NavBar>
+        </>
+    )
+}
+export default Navbar
---
+++ front-end/src/Pages/Admin/ListagemEstacoes/styles.ts
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export const ListagemEstacao = styled.div`
+section{
+    main{
+        h1{
+            margin-top:0px;
+            display:flex;
+            justify-content:center;
+            color:#5652C7;
+            font-size: 50px;
+        }
+        .pesquisa{
+            padding: 20px;
+            border: 1px solid red;
+            p-autocomplete{
+                width: auto;
+            }
+        }
+    }
+}
+`
